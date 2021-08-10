3f007e130f1bc577b01fc3b2c38f6a400f95781c
David Evans <davidje13@users.noreply.github.com>
2021-08-10 19:34:04 +0100

    Prevent multiple simultaneous auto-players

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-async function playSlow(game, player, moveDelay) {
+async function playSlow(game, player, moveDelay, checkActive) {
   for (const move of player.play(game)) {
+    if (!checkActive()) {
+      return;
+    }
     switch (move.action) {
       case 'flag': game.toggleFlag(move.id); break;
       case 'clear': game.revealCell(move.id); break;
@@ -16,15 +19,24 @@ window.addEventListener('DOMContentLoaded', () => {
   const display = new MinesweeperDisplay();
   document.body.appendChild(display.base);
   let game = null;
+  let playing = null;
 
   function begin() {
     game = new MinesweeperGame(grid, 300, { freePasses: 5 });
     display.display(game);
+    playing = null;
   }
 
   display.addEventListener('reset', begin);
   display.addEventListener('secret-autoplay', () => {
-    playSlow(game, player, 50);
+    if (playing !== game) {
+      playing = game;
+      playSlow(game, player, 50, () => (playing === game)).then(() => {
+        if (playing === game) {
+          playing = null;
+        }
+      });
+    }
   });
   begin();
 });
